6ab6f0ecbef1cfa899e55df378f318ea9e555902
Joel <yantornojoel@gmail.com>
2020-09-02 14:27:06 -0300

    Updates

---
+++ precache-manifest.e99d7cf4d045bbfe701a8d5e22d30544.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eaad07feef947fba0b5c3fcfd8518a4",
+    "url": "/NodeMongoLoginNotes/index.html"
+  },
+  {
+    "revision": "7135564426fc9c5b1dc8",
+    "url": "/NodeMongoLoginNotes/static/css/2.d98961f1.chunk.css"
+  },
+  {
+    "revision": "2d375e02e95a82b094f2",
+    "url": "/NodeMongoLoginNotes/static/css/main.0067cc6a.chunk.css"
+  },
+  {
+    "revision": "7135564426fc9c5b1dc8",
+    "url": "/NodeMongoLoginNotes/static/js/2.57fd8b3a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/NodeMongoLoginNotes/static/js/2.57fd8b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d375e02e95a82b094f2",
+    "url": "/NodeMongoLoginNotes/static/js/main.47529d24.chunk.js"
+  },
+  {
+    "revision": "54122215c07b013a91b1",
+    "url": "/NodeMongoLoginNotes/static/js/runtime-main.ddde31fe.js"
+  }
+]);
